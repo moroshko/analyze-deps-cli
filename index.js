d52d69b8ca96659ee3e9490de7a7dacc162264dd
Misha Moroshko <michael.moroshko@gmail.com>
2016-10-14 00:07:36 +1100

    Start working on interactive update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const chalk = require('chalk');
 const table = require('text-table');
 const minimist = require('minimist');
-const stripAnsi = require('strip-ansi');
+const inquirer = require('inquirer');
 const sortBy = require('lodash.sortby');
 const fileExists = require('file-exists');
 const logSymbols = require('log-symbols');
@@ -40,36 +40,122 @@ if (specifiedPackageJsonLocation) {
 console.log(chalk.magenta(`Analyzing ${path.relative(process.cwd(), packageJsonPath)}`)); // eslint-disable-line no-console
 
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-const calcColoredStringLength = str => stripAnsi(str).length;
-const headers = [
-  chalk.cyan.underline('package'),
-  chalk.red.underline('current'),
-  chalk.green.underline('latest'),
-  ''
-];
+
+const calcColoredStringLength = str => chalk.stripColor(str).length;
 
 const getSortKey = analysis => {
   switch (analysis.diff) {
-    case 'major': return 1;
-    case 'minor': return 2;
-    case 'patch': return 3;
-    case 'premajor': return 4;
-    case 'preminor': return 5;
-    case 'prepatch': return 6;
+    case 'patch': return 1;
+    case 'prepatch': return 2;
+    case 'minor': return 3;
+    case 'preminor': return 4;
+    case 'major': return 5;
+    case 'premajor': return 6;
     case 'prerelease': return 7;
     default: return 8;
   }
 };
 
-const diffColorMap = { // Traffic light here :)
-  major: 'red',
+const diffColorMap = {
+  patch: 'green',
   minor: 'yellow',
-  patch: 'green'
+  major: 'red'
 };
 
 const colorizeDiff = diff =>
   diffColorMap[diff] ? chalk[diffColorMap[diff]](diff) : diff;
 
+const showErrors = analysis => {
+  let notLatest = {};
+
+  for (let depKey in analysis) {
+    const deps = analysis[depKey];
+
+    notLatest[depKey] = notLatest[depKey] || {};
+
+    for (let packageName in deps) {
+      const packageAnalysis = deps[packageName];
+      const status = packageAnalysis.status;
+
+      if (status === 'error') {
+        printError(packageAnalysis.error);
+      } else if (status === 'not-latest') {
+        notLatest[depKey][packageName] = packageAnalysis;
+      }
+    }
+  }
+
+  return notLatest;
+};
+
+const headerMap = {
+  dependencies: 'dep',
+  devDependencies: 'devDep'
+};
+
+const showPrompt = notLatest => {
+  let rows = [], headerIndices = {};
+
+  for (let key in notLatest) {
+    const deps = notLatest[key];
+    const header = [
+      chalk.reset(chalk.cyan.underline.bold(headerMap[key])),
+      chalk.reset(chalk.red.underline.bold('current')),
+      chalk.reset(chalk.green.underline.bold('latest')),
+      ''
+    ];
+    const body = sortBy(Object.keys(deps), packageName => getSortKey(deps[packageName]))
+      .map(packageName => {
+        const analysis = deps[packageName];
+
+        return [
+          packageName,
+          analysis.current,
+          analysis.latest,
+          colorizeDiff(analysis.diff)
+        ];
+      });
+
+    headerIndices[rows.length] = true;
+    rows = rows.concat([header], body);
+  }
+
+  rows = table(rows, { stringLength: calcColoredStringLength });
+
+  const choices = rows.split('\n').map((row, index) => {
+    if (headerIndices[index]) {
+      return new inquirer.Separator(`  ${row}`);
+    }
+
+    return {
+      name: row,
+      value: {
+        key: 'todo',
+        packageName: 'todo'
+      },
+      short: 'todo'
+    };
+  });
+  const question = {
+    type: 'checkbox',
+    message: 'Select packages to update',
+    name: 'updates',
+    choices: choices
+  };
+
+  return inquirer.prompt([question]);
+};
+
+const updatePackageJson = selection => {
+  return selection;
+};
+
+analyzeDeps(packageJson)
+  .then(showErrors)
+  .then(showPrompt)
+  .then(updatePackageJson);
+
+/*
 const printAnalysis = analysis =>
   ['dependencies', 'devDependencies'].forEach(key => {
     const singleAnalysis = analysis[key];
@@ -114,3 +200,4 @@ const printAnalysis = analysis =>
   });
 
 analyzeDeps(packageJson).then(printAnalysis);
+*/
